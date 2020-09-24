1f7b488e85f0cc5ca55b185d04059df957cedfab
s-safak <ssafak@gmail.com>
2020-09-25 01:14:53 +0300

    Add files via upload

---
+++ TCP_Server_Multi/TCP_Server_Multi/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace TCP_Server_Multi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.listBox_Receive = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_Folder = new System.Windows.Forms.Label();
+            this.button_Folder = new System.Windows.Forms.Button();
+            this.comboBox_Host = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(79, 60);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(55, 20);
+            this.textBox_port.TabIndex = 1;
+            this.textBox_port.Text = "5656";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Host IP : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port : ";
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(199, 57);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 3;
+            this.button_Start.Text = "Start Server";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Enabled = false;
+            this.button_Stop.Location = new System.Drawing.Point(199, 86);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(75, 23);
+            this.button_Stop.TabIndex = 3;
+            this.button_Stop.Text = "Stop Server";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 383);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(491, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // listBox_Receive
+            // 
+            this.listBox_Receive.FormattingEnabled = true;
+            this.listBox_Receive.Location = new System.Drawing.Point(307, 31);
+            this.listBox_Receive.Name = "listBox_Receive";
+            this.listBox_Receive.Size = new System.Drawing.Size(183, 186);
+            this.listBox_Receive.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Received Files";
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.BackColor = System.Drawing.Color.Black;
+            this.textBox_log.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_log.ForeColor = System.Drawing.Color.White;
+            this.textBox_log.Location = new System.Drawing.Point(0, 115);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.Size = new System.Drawing.Size(301, 105);
+            this.textBox_log.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Server Status : ";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_Status.ForeColor = System.Drawing.Color.Red;
+            this.label_Status.Location = new System.Drawing.Point(76, 96);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(58, 13);
+            this.label_Status.TabIndex = 25;
+            this.label_Status.Text = "OFFLINE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Receive Folder : ";
+            // 
+            // label_Folder
+            // 
+            this.label_Folder.AutoSize = true;
+            this.label_Folder.Location = new System.Drawing.Point(94, 9);
+            this.label_Folder.Name = "label_Folder";
+            this.label_Folder.Size = new System.Drawing.Size(23, 13);
+            this.label_Folder.TabIndex = 2;
+            this.label_Folder.Text = "D:\\";
+            // 
+            // button_Folder
+            // 
+            this.button_Folder.Location = new System.Drawing.Point(199, 9);
+            this.button_Folder.Name = "button_Folder";
+            this.button_Folder.Size = new System.Drawing.Size(75, 23);
+            this.button_Folder.TabIndex = 26;
+            this.button_Folder.Text = "Select";
+            this.button_Folder.UseVisualStyleBackColor = true;
+            this.button_Folder.Click += new System.EventHandler(this.button_Folder_Click);
+            // 
+            // comboBox_Host
+            // 
+            this.comboBox_Host.FormattingEnabled = true;
+            this.comboBox_Host.Location = new System.Drawing.Point(79, 33);
+            this.comboBox_Host.Name = "comboBox_Host";
+            this.comboBox_Host.Size = new System.Drawing.Size(100, 21);
+            this.comboBox_Host.TabIndex = 27;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 405);
+            this.Controls.Add(this.comboBox_Host);
+            this.Controls.Add(this.button_Folder);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_log);
+            this.Controls.Add(this.listBox_Receive);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_Folder);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_port);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multi Thread TCP Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ListBox listBox_Receive;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_Status;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_Folder;
+        private System.Windows.Forms.Button button_Folder;
+        private System.Windows.Forms.ComboBox comboBox_Host;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TCP_Server_Multi/TCP_Server_Multi/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Server_Multi
+{
+    public partial class Form1 : Form
+    {
+        Thread t1;
+        int flag = 0;
+        string receivedPath = "";
+        string host_ = "";
+        int port_ = 0;
+        int t_check = 0;
+        public delegate void MyDelegate();
+        private string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class StateObject
+        {
+            // Client socket.
+            public Socket workSocket = null;
+
+            public const int BufferSize = 8096;
+            // Receive buffer.
+            public byte[] buffer = new byte[BufferSize];
+        }
+
+        public static ManualResetEvent allDone = new ManualResetEvent(true);
+
+        public void StartListening()
+        {
+            byte[] bytes = new Byte[8096];
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse(host_), port_);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(ipEnd);
+                listener.Listen(100);
+                logyaz("Listening For Connection");
+                while (true)
+                {
+                    allDone.Reset();
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    allDone.WaitOne();
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+            flag = 0;
+
+        }
+
+        public void ReadCallback(IAsyncResult ar)
+        {
+            int fileNameLen = 1;
+            String content = String.Empty;
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead > 0)
+            {
+                if (flag == 0)
+                {
+                    fileNameLen = BitConverter.ToInt32(state.buffer, 0);
+                    fileName = Encoding.UTF8.GetString(state.buffer, 4, fileNameLen);
+                    receivedPath = @"D:\" + fileName;
+                    flag++;
+                }
+
+                if (flag >= 1)
+                {
+                    BinaryWriter writer = new BinaryWriter(File.Open(receivedPath, FileMode.Append));
+                    if (flag == 1)
+                    {
+                        writer.Write(state.buffer, 4 + fileNameLen, bytesRead - (4 + fileNameLen));
+                        flag++;
+                    }
+                    else
+                        writer.Write(state.buffer, 0, bytesRead);
+                    writer.Close();
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+            }
+            else
+            {
+                Invoke(new MyDelegate(LabelWriter));
+            }
+
+        }
+
+        public void LabelWriter()
+        {
+            label1.Text = "Data has been received " + fileName;
+        }
+
+        private void logyaz(string mesaj)
+        {
+
+            textBox_log.Text = Environment.NewLine + DateTime.Now.ToString() + "..." + mesaj.TrimEnd().ToString() + "......" + textBox_log.Text.TrimEnd().ToString();
+
+            if (textBox_log.Text.TrimEnd().ToString().Length >= 32767) textBox_log.Clear();
+            textBox_log.Refresh();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Get host name
+            String strHostName = Dns.GetHostName();
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+            comboBox_Host.Items.Add("127.0.0.1");
+            foreach(IPAddress ipaddress in iphostentry.AddressList)
+            {
+                comboBox_Host.Items.Add(ipaddress.ToString());
+            }
+            receivedPath = label_Folder.Text;
+            comboBox_Host.SelectedIndex = 0;
+        }
+
+        private void button_Folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                label_Folder.Text = fd.SelectedPath;
+                receivedPath = label_Folder.Text;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            t1.Abort();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //TcpClient tcpCheckClient = new TcpClient();
+            //tcpCheckClient.Connect(host_, port_);
+            if (t1.IsAlive){
+                
+                label_Status.Text = "ONLINE";
+                button_Start.Enabled = false;
+                button_Stop.Enabled = true;
+                if (t_check == 0)
+                {
+                    logyaz(host_ + ":" + textBox_port.Text + "OPEN");
+                }
+                t_check++;
+            } else {
+                label_Status.Text = "OFFLINE";
+                button_Start.Enabled = true;
+                button_Stop.Enabled = false;
+                logyaz(host_ + ":" + textBox_port.Text + "CLOSED");
+                timer1.Enabled = false;
+            }
+        }
+//START
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            host_ = comboBox_Host.Text;
+            port_ = Convert.ToInt32(textBox_port.Text);
+            label_Status.Text = "ONLINE";
+            button_Start.Enabled = false;
+            button_Stop.Enabled = true;
+            t1 = new Thread(new ThreadStart(StartListening));
+            t1.Start();
+            logyaz(host_ + ":" + textBox_port.Text + "OPEN");
+            //timer1.Enabled = true;
+        }
+//STOP
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            t1.Abort();
+            label_Status.Text = "OFFLINE";
+            button_Start.Enabled = true;
+            button_Stop.Enabled = false;
+            logyaz(host_ + ":" + textBox_port.Text + "CLOSED");
+        }
+
+    }
+}
